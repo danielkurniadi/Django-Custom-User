b95830d67c6a447034dfca45405d8ae5eca1fbeb
iqDF <daniel.thekurniadi@gmail.com>
2019-06-07 21:03:21 +0800

    Duty - Urls add new path for duty pages

--- duty_api/urls.py
+++ duty_api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import duty_handler
+from .views import duty_handler, duty_view
 
 urlpatterns = [
-    path('api/', duty_handler, name='duty'),
+    path('', duty_view, name='duty-page'),
+    path('api/', duty_handler, name='duty-api'),
 ]
